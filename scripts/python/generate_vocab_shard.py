3b8429bc2b3fa59746f54f6da735f12b82ef4c6f
wylie102 <wylie102@gmail.com>
2025-05-02 16:19:41 +0100

    Finished altering vocab shard

--- scripts/python/generate_vocab_shard.py
+++ scripts/python/generate_vocab_shard.py
@@ -45,59 +45,54 @@ non_vocab_tables: list[str] = [
     "visit_detail",
     "visit_occurrence",
 ]
-concept_ids: list[int] = []
-conn.sql("CREATE TEMPORARY TABLE cids(concept_id INTEGER);")
-duck_concept_ids = conn.table(cids)
+
+_ = conn.sql("CREATE TEMPORARY TABLE cids(concept_id INTEGER);")
+duck_concept_ids = conn.table("cids")
 
 # TODO: add unit concepts found in drug_strength table
 for table in non_vocab_tables:
     print(f"Searching table {table}")
+    sql_non_vocab = f'SELECT "name" FROM pragma_table_info({source_schema}.{table}) WHERE "name" LIKE \'%_concept_id\';'
     field_tuples: list[tuple[str]] = cast(
-        "list[tuple[str]]",
-        conn.sql(
-            f'SELECT "name" from (PRAGMA table_info({source_schema}.{table}) WHERE "name" like \'%_concept_id\';'
-        ).fetchall(),
+        "list[tuple[str]]", conn.sql(sql_non_vocab).fetchall()
     )
-    fields: list[str] = [field for (field,) in field_tuples]
-    for field in fields:
+    non_vocab_fields: list[str] = [field for (field,) in field_tuples]
+    for field in non_vocab_fields:
         print(f"- Searching field {field}")
-        sql: str = f"SELECT DISTINCT {field} FROM {source_schema}.{table};"
-        id_tuples: list[tuple[int]] = cast("list[tuple[int]]", conn.sql(sql).fetchall())
-        concept_ids.extend([id for (id,) in id_tuples if (id,) is not None])
+        concept_ids = conn.sql(f"SELECT DISTINCT {field} FROM {source_schema}.{table};")
+        concept_ids.insert_into("cids")
+
 
 # Expand with all parents
-conn.sql("CREATE TEMPORARY TABLE cids(concept_id INTEGER);")
-conn.sql(f"INSERT INTO cids(concept_id) VALUES ({concept_ids});")
-
-sql = f"""SELECT DISTINCT ancestor_concept_id 
+sql_ancestor: str = f"""SELECT DISTINCT ancestor_concept_id 
 FROM {source_schema}.concept_ancestor 
 INNER JOIN cids
   ON descendant_concept_id = concept_id;"""
-concept_ids.extend(conn.sql(sql).fetchall())
-concept_ids = list(set(concept_ids))
-
-# Filter data to selected concept IDs
-for concept_id in concept_ids:
-    if concept_id[0] is not None:
-        conn.sql(f"INSERT INTO cids(concept_id) VALUES ({concept_id[0]});")
+expanded_ids = conn.sql(sql_ancestor)
+expanded_ids.insert_into("cids")
 
 # Filter vocab tables
-conn.sql(f"CREATE SCHEMA IF NOT EXISTS {target_schema}")
+_ = conn.sql(f"CREATE SCHEMA IF NOT EXISTS {target_schema}")
 for table in vocab_tables:
     print(f"Filtering table {table}")
-    conn.sql(f"DROP TABLE IF EXISTS {target_schema}.{table};")
-    fields = conn.sql(f"PRAGMA table_info({source_schema}.{table});").fetchall()
-    fields = [field[1] for field in fields if "concept_id" in field[1]]
-    sql = f"CREATE TABLE {target_schema}.{table} AS SELECT * FROM {source_schema}.{table} WHERE "
-    sql += " AND ".join(
-        [f"{field} IN (SELECT concept_id FROM cids)" for field in fields]
+    _ = conn.sql(f"DROP TABLE IF EXISTS {target_schema}.{table};")
+    vocab_fields_tuples: list[str] = cast(
+        "list[str]",
+        conn.sql(
+            f'SELECT "name" from pragma_table_info({source_schema}.{table}) WHERE "name" LIKE \'concept_id\';'
+        ).fetchall(),
     )
-    conn.sql(sql)
+    vocab_fields: list[str] = [field for field in vocab_fields_tuples]
+    sql_vocab: str = f"CREATE TABLE {target_schema}.{table} AS SELECT * FROM {source_schema}.{table} WHERE "
+    sql_vocab += " AND ".join(
+        [f"{field} IN (SELECT concept_id FROM cids)" for field in vocab_fields]
+    )
+    _ = conn.sql(sql_vocab)
 
 for table in vocab_tables_preserve:
     print(f"Migrating table {table}")
-    conn.sql(f"DROP TABLE IF EXISTS {target_schema}.{table};")
-    sql = f"CREATE TABLE {target_schema}.{table} AS SELECT * FROM {source_schema}.{table} "
-    conn.sql(sql)
+    _ = conn.sql(f"DROP TABLE IF EXISTS {target_schema}.{table};")
+    sql_vocab = f"CREATE TABLE {target_schema}.{table} AS SELECT * FROM {source_schema}.{table} "
+    _ = conn.sql(sql_vocab)
 
 conn.close()
